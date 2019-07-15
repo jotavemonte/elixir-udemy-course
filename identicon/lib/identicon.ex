912289de041842f09b16be9e0b84e550efb8ad21
João Victor Monte <jotavemonte@gmail.com>
2019-07-15 06:12:30 -0300

    finished identicon lesson

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,13 +5,63 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+    :ok
   end
 
-  def build_grid(%Identicon.Image{hex: hex} = _image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
-    |> List.flatten
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def get_position(index)do
+    x_init = rem(index, 5) * 50
+    y_init = div(index, 5) * 50
+    x_final = x_init + 50
+    y_final = y_init + 50
+    {{x_init, y_init}, {x_final, y_final}}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      get_position(index)
+    end
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = grid
+    |> Enum.filter(
+        fn({code, _index}) ->
+          rem(code, 2) == 0
+        end
+      )
+
+      %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
@@ -22,11 +72,11 @@ defmodule Identicon do
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+    |> List.delete_at(-1)
     %Identicon.Image{hex: hex}
   end
 
     def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
-
 end
